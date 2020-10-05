22284c80618a6a30065af07b63202abbb7edce16
abdul kareem <as.abdulkareem@gmail.com>
2020-10-06 00:02:43 +0530

    Updates

---
+++ precache-manifest.4e6859736b7eb6bdef02684798bc60a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c273f320e75d6547608ba5483639c66",
+    "url": "/clothshop/index.html"
+  },
+  {
+    "revision": "3c3e23e168639790adab",
+    "url": "/clothshop/static/css/main.18a3ac86.chunk.css"
+  },
+  {
+    "revision": "68cfd06f5102d95b1fd7",
+    "url": "/clothshop/static/js/2.8f4abbc3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clothshop/static/js/2.8f4abbc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c3e23e168639790adab",
+    "url": "/clothshop/static/js/main.c519213d.chunk.js"
+  },
+  {
+    "revision": "008be9c8b46461baad46",
+    "url": "/clothshop/static/js/runtime-main.fa14485d.js"
+  }
+]);
